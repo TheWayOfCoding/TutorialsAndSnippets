6757416f2dace8e29a4bae6abf96dd295479e760
Scott <sjw17@yahoo.com>
2016-04-30 19:19:06 -0500

    Added a c# example.

---
+++ CSharp/BackgroundWorkerExample/BackgroundWorkerExample/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace BackgroundWorkerExample
+{
+    partial class frmBackgroundWorkerExample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bwInstance = new System.ComponentModel.BackgroundWorker();
+            this.pbLoadingGraphic = new System.Windows.Forms.PictureBox();
+            this.lblLoadingStatus = new System.Windows.Forms.Label();
+            this.prgLoadingProgress = new System.Windows.Forms.ProgressBar();
+            this.btnStartWorker = new System.Windows.Forms.Button();
+            this.btnStopWorker = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLoadingGraphic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bwInstance
+            // 
+            this.bwInstance.WorkerReportsProgress = true;
+            this.bwInstance.WorkerSupportsCancellation = true;
+            this.bwInstance.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bwInstance_DoWork);
+            this.bwInstance.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bwInstance_ProgressChanged);
+            this.bwInstance.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bwInstance_RunWorkerCompleted);
+            // 
+            // pbLoadingGraphic
+            // 
+            this.pbLoadingGraphic.Image = global::BackgroundWorkerExample.Properties.Resources.Loading_2_transparent;
+            this.pbLoadingGraphic.Location = new System.Drawing.Point(413, 12);
+            this.pbLoadingGraphic.Name = "pbLoadingGraphic";
+            this.pbLoadingGraphic.Size = new System.Drawing.Size(70, 70);
+            this.pbLoadingGraphic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbLoadingGraphic.TabIndex = 0;
+            this.pbLoadingGraphic.TabStop = false;
+            this.pbLoadingGraphic.Visible = false;
+            // 
+            // lblLoadingStatus
+            // 
+            this.lblLoadingStatus.AutoSize = true;
+            this.lblLoadingStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoadingStatus.Location = new System.Drawing.Point(12, 51);
+            this.lblLoadingStatus.Name = "lblLoadingStatus";
+            this.lblLoadingStatus.Size = new System.Drawing.Size(134, 31);
+            this.lblLoadingStatus.TabIndex = 1;
+            this.lblLoadingStatus.Text = "Loading...";
+            this.lblLoadingStatus.Visible = false;
+            // 
+            // prgLoadingProgress
+            // 
+            this.prgLoadingProgress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.prgLoadingProgress.Location = new System.Drawing.Point(12, 110);
+            this.prgLoadingProgress.Name = "prgLoadingProgress";
+            this.prgLoadingProgress.Size = new System.Drawing.Size(471, 23);
+            this.prgLoadingProgress.TabIndex = 2;
+            // 
+            // btnStartWorker
+            // 
+            this.btnStartWorker.Location = new System.Drawing.Point(12, 12);
+            this.btnStartWorker.Name = "btnStartWorker";
+            this.btnStartWorker.Size = new System.Drawing.Size(75, 23);
+            this.btnStartWorker.TabIndex = 3;
+            this.btnStartWorker.Text = "Start Worker";
+            this.btnStartWorker.UseVisualStyleBackColor = true;
+            this.btnStartWorker.Click += new System.EventHandler(this.btnStartWorker_Click);
+            // 
+            // btnStopWorker
+            // 
+            this.btnStopWorker.Enabled = false;
+            this.btnStopWorker.Location = new System.Drawing.Point(94, 12);
+            this.btnStopWorker.Name = "btnStopWorker";
+            this.btnStopWorker.Size = new System.Drawing.Size(75, 23);
+            this.btnStopWorker.TabIndex = 4;
+            this.btnStopWorker.Text = "Stop Worker";
+            this.btnStopWorker.UseVisualStyleBackColor = true;
+            this.btnStopWorker.Click += new System.EventHandler(this.btnStopWorker_Click);
+            // 
+            // frmBackgroundWorkerExample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 145);
+            this.Controls.Add(this.btnStopWorker);
+            this.Controls.Add(this.btnStartWorker);
+            this.Controls.Add(this.prgLoadingProgress);
+            this.Controls.Add(this.lblLoadingStatus);
+            this.Controls.Add(this.pbLoadingGraphic);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmBackgroundWorkerExample";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Background Worker Example - TheWayOfCoding.com";
+            ((System.ComponentModel.ISupportInitialize)(this.pbLoadingGraphic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker bwInstance;
+        private System.Windows.Forms.PictureBox pbLoadingGraphic;
+        private System.Windows.Forms.Label lblLoadingStatus;
+        private System.Windows.Forms.ProgressBar prgLoadingProgress;
+        private System.Windows.Forms.Button btnStartWorker;
+        private System.Windows.Forms.Button btnStopWorker;
+    }
+}
+
---
+++ CSharp/BackgroundWorkerExample/BackgroundWorkerExample/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackgroundWorkerExample
+{
+    public partial class frmBackgroundWorkerExample : Form
+    {
+        public enum CurrentStatus
+        {
+            None,
+            Reset,
+            Loading,
+            Cancelled,
+            Success,
+            Busy
+        }
+
+        public frmBackgroundWorkerExample()
+        {
+            InitializeComponent();
+        }
+
+        //used throughout the form to track application status
+        CurrentStatus processingStatus = CurrentStatus.None;
+
+        /// <summary>
+        /// changes the interface based on current status (processingStatus)
+        /// </summary>
+        void setFormControlsBasedOnStatus()
+        {
+            switch (processingStatus)
+            {
+                case CurrentStatus.None:
+                    break;
+                case CurrentStatus.Reset:
+                    btnStartWorker.Enabled = true;
+                    btnStopWorker.Enabled = false;
+                    lblLoadingStatus.Visible = false;
+                    lblLoadingStatus.Text = "Loading...";
+                    pbLoadingGraphic.Visible = false;
+                    prgLoadingProgress.Value = 0;
+                    break;
+
+                case CurrentStatus.Loading:
+                    btnStartWorker.Enabled = false;
+                    btnStopWorker.Enabled = true;
+                    lblLoadingStatus.Visible = true;
+                    lblLoadingStatus.Text = "Loading...";
+                    pbLoadingGraphic.Visible = true;
+                    prgLoadingProgress.Value = 0;
+                    break;
+
+                case CurrentStatus.Cancelled:
+                    btnStartWorker.Enabled = true;
+                    btnStopWorker.Enabled = false;
+                    lblLoadingStatus.Visible = true;
+                    lblLoadingStatus.Text = "Cancelled";
+                    pbLoadingGraphic.Visible = false;
+                    prgLoadingProgress.Value = 0;
+                    break;
+
+                case CurrentStatus.Success:
+                    btnStartWorker.Enabled = true;
+                    btnStopWorker.Enabled = false;
+                    lblLoadingStatus.Visible = true;
+                    lblLoadingStatus.Text = "Success!";
+                    pbLoadingGraphic.Visible = false;
+                    prgLoadingProgress.Value = 0;
+                    break;
+
+                case CurrentStatus.Busy:
+                    btnStartWorker.Enabled = true;
+                    btnStopWorker.Enabled = false;
+                    lblLoadingStatus.Visible = true;
+                    lblLoadingStatus.Text = "The worker is busy.";
+                    pbLoadingGraphic.Visible = false;
+                    prgLoadingProgress.Value = 0;
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+
+        /// <summary>
+        /// see if the user wants to cancel the background worker
+        /// </summary>
+        /// <param name="worker">the worker</param>
+        /// <param name="e">worker status</param>
+        /// <returns>used to tell the caller to exit or not</returns>
+        bool isWorkerBeingCanceled(BackgroundWorker worker, DoWorkEventArgs e)
+        {
+            bool returnValue = false;
+
+            if (worker.CancellationPending == true)
+            {
+                e.Cancel = true;
+                returnValue = true;
+                processingStatus = CurrentStatus.Cancelled;
+            }
+
+            return returnValue;
+        }
+
+        /// <summary>
+        /// background worker processing step
+        /// </summary>
+        /// <param name="sender">worker instance</param>
+        /// <param name="e">current status</param>
+        private void bwInstance_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            //mimic work and update or cancel the worker as we go
+            if (isWorkerBeingCanceled(worker, e)) return; //check for user cancel
+            worker.ReportProgress(20);
+            System.Threading.Thread.Sleep(250);
+
+            //mimic work and update or cancel the worker as we go
+            if (isWorkerBeingCanceled(worker, e)) return; //check for user cancel
+            worker.ReportProgress(40);
+            System.Threading.Thread.Sleep(250);
+
+            //mimic work and update or cancel the worker as we go
+            if (isWorkerBeingCanceled(worker, e)) return; //check for user cancel
+            worker.ReportProgress(60);
+            System.Threading.Thread.Sleep(250);
+
+            //mimic work and update or cancel the worker as we go
+            if (isWorkerBeingCanceled(worker, e)) return; //check for user cancel
+            worker.ReportProgress(80);
+            System.Threading.Thread.Sleep(250);
+
+            //mimic work and update or cancel the worker as we go
+            if (isWorkerBeingCanceled(worker, e)) return; //check for user cancel
+            worker.ReportProgress(100);
+            System.Threading.Thread.Sleep(250);
+
+            processingStatus = CurrentStatus.Success;
+        }
+
+        /// <summary>
+        /// updates any progress controls visually
+        /// </summary>
+        /// <param name="sender">current instance</param>
+        /// <param name="e">passed progress</param>
+        private void bwInstance_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            prgLoadingProgress.Value = e.ProgressPercentage;
+        }
+
+        /// <summary>
+        /// happens once the background worker is completed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bwInstance_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            setFormControlsBasedOnStatus();
+        }
+
+        /// <summary>
+        /// a button to start processing
+        /// </summary>
+        private void btnStartWorker_Click(object sender, EventArgs e)
+        {
+            if (bwInstance.IsBusy == false)
+            {
+                processingStatus = CurrentStatus.Loading;
+                setFormControlsBasedOnStatus();
+                bwInstance.RunWorkerAsync();
+            }
+            else
+            {
+                processingStatus = CurrentStatus.Busy;
+                setFormControlsBasedOnStatus();
+            }
+        }
+
+        /// <summary>
+        /// a button to stop processing
+        /// </summary>
+        private void btnStopWorker_Click(object sender, EventArgs e)
+        {
+            bwInstance.CancelAsync();
+        }
+    }
+}
