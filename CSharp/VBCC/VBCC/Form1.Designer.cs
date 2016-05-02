0449b71ff801bc6e616e1ede74d26f77847b525a
Scott <sjw17@yahoo.com>
2016-05-02 00:12:51 -0500

    Added a simple project that counts classic VB code.

---
+++ CSharp/VBCC/VBCC/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace VBCC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.folderSelector = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtLineCount = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Process Project Directory...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Location = new System.Drawing.Point(12, 41);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(641, 460);
+            this.txtResult.TabIndex = 1;
+            this.txtResult.WordWrap = false;
+            // 
+            // folderSelector
+            // 
+            this.folderSelector.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // txtLineCount
+            // 
+            this.txtLineCount.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLineCount.Location = new System.Drawing.Point(172, 14);
+            this.txtLineCount.Name = "txtLineCount";
+            this.txtLineCount.Size = new System.Drawing.Size(481, 20);
+            this.txtLineCount.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 513);
+            this.Controls.Add(this.txtLineCount);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Classic VB Code Counter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.FolderBrowserDialog folderSelector;
+        private System.Windows.Forms.TextBox txtLineCount;
+    }
+}
+
---
+++ CSharp/VBCC/VBCC/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+/*
+By: Scott Waldron
+TheWayOfCoding.com
+
+- All resources retain copyright (bitmaps, etc).
+- You are not allowed to use these projects 
+as tutorials or samples distributed with your own branding.
+Basically, any use with the same theme as they were 
+intended for with TheWayOfCoding.com is not allowed.
+
+The MIT License (MIT)
+
+Copyright (c) 2016 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+namespace VBCC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public IEnumerable<string> FilterFiles(string path, params string[] exts)
+        {
+            return exts.Select(x => "*." + x).SelectMany(x => Directory.EnumerateFiles(path, x));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //only parse VB6 frm, cls, and bas files
+            var validExtensions = new[] { "frm", "cls", "bas" };
+
+            txtResult.Text = "";
+            txtLineCount.Text = "";
+
+            if (folderSelector.ShowDialog() == DialogResult.OK)
+            {
+                //only parse files that end in our desired extensions
+                IEnumerable<string> foundFiles = FilterFiles(folderSelector.SelectedPath, validExtensions);
+
+                //loop through all of the files so we can count lines of code per project folder
+                foreach (string value in foundFiles)
+                {
+                    string currentFile = File.ReadAllText(value);
+
+                    //clear out any lines that are entirely comments
+                    currentFile = Regex.Replace(currentFile, @"'(.*)", "", RegexOptions.Multiline);
+
+                    //clear out any lines that are blank
+                    currentFile = Regex.Replace(currentFile, @"^\s+$[\r\n]*", "", RegexOptions.Multiline);
+
+                    //remove any blocks of empty spaces so that there are only single spaces
+                    while (currentFile.Contains("  "))
+                    {
+                        currentFile = currentFile.Replace("  ", " ");
+                    }
+
+                    //confirm there are not empty lines
+                    while (currentFile.Contains("\n\n"))
+                    {
+                        currentFile = currentFile.Replace("\n\n", "\n");
+                    }
+                    
+                    //append the parsed code file text
+                    txtResult.Text += currentFile;
+                }
+
+                //count the project's entire lines of code
+                txtLineCount.Text = (txtResult.Lines.Count() - 1).ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
